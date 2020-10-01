2179099daff5b359a1dad1f696b6d04aa1e006a5
Changhoon Choi <56301358+h1on@users.noreply.github.com>
2020-10-01 18:54:49 +0900

    Add Logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <div>
+            <header>
+                <ul className="navbar">
+                    <li className="nav-item">
+                        <a className="active" href="/">
+                            pr0gr4m
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/browse">둘러보기</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/storage">보관함</a>
+                    </li>
+                </ul>
+            </header>
+        </div>
+    )
 }
 
 export default App
